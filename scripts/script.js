f9ff68ef97ff58d3dad450114ed46794a748d064
EmanuelBatixta <emanueljonatas28@gmail.com>
2025-01-01 20:24:28 -0300

    update galeria

--- scripts/script.js
+++ scripts/script.js
@@ -71,38 +71,37 @@ async function fetchProducts() {
         console.error('Erro ao carregar o arquivo JSON:', error);
     } 
 }
+ 
+async function showList() {
+    const gifts = await fetchProducts();
 
-if(list){
-    
-    async function showList() {
-        const gifts = await fetchProducts();
+    gifts.forEach(gift => {
+        const card = document.createElement("div")
+        const name = document.createElement("p")
+        const price = document.createElement("p")
+        const img = document.createElement("img")
+        const btn = document.createElement("a")
 
-        gifts.forEach(gift => {
-            const card = document.createElement("div")
-            const name = document.createElement("p")
-            const price = document.createElement("p")
-            const img = document.createElement("img")
-            const btn = document.createElement("a")
+        img.setAttribute("loading","lazy")
+        img.setAttribute("alt", gift.nome)
+        img.setAttribute("src", gift.img)
 
-            img.setAttribute("loading","lazy")
-            img.setAttribute("alt", gift.nome)
-            img.setAttribute("src", gift.img)
+        name.textContent = gift.nome
+        price.innerHTML = `Por <h2>R$ ${gift.valor}</h2>`
+        btn.innerHTML = `Presentear <img src="images/market.svg" loading="lazy" alt="cart">`
+        
+        card.setAttribute("class","item")
+        price.setAttribute("id","price")
 
-            name.textContent = gift.nome
-            price.innerHTML = `Por <h2>R$ ${gift.valor}</h2>`
-            btn.innerHTML = `Presentear <img src="images/market.svg" loading="lazy" alt="cart">`
-            
-            card.setAttribute("class","item")
-            price.setAttribute("id","price")
+        card.appendChild(img)
+        card.appendChild(name)
+        card.appendChild(price)
+        card.appendChild(btn)
 
-            card.appendChild(img)
-            card.appendChild(name)
-            card.appendChild(price)
-            card.appendChild(btn)
-
-            list.appendChild(card)
-        });
-    }
+        list.appendChild(card)
+    });
 }
 
-showList();
+if(list){
+    showList();
+}
